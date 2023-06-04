towersvault/shinpokered

- The silhouette effect at the start of battle now displays when playing on a DMG gameboy

engine/menu/main_menu.asm
@@ -418,7 +418,7 @@ HandshakeList:
 	db $a
 	db $ff
 VersionText:
-	db "v1.23.14M0510230955@"
+	db "v1.23.14M0519231215@"
 
 WhereWouldYouLikeText:
 	TX_FAR _WhereWouldYouLikeText

tournepivoine/shinpokered-fr

engine/menu/main_menu.asm
@@ -393,7 +393,7 @@ HandshakeList:	;this serves as a version control passcode with FF as an end-of-l
 	db $b
 	db $ff
 VersionText:
-	db "v1.23.14L0510230955@"
+	db "v1.23.14L0519231215@"
 
 WhereWouldYouLikeText:
 	TX_FAR _WhereWouldYouLikeText