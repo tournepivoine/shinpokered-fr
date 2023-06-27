18dd9715d0a00f621caf128cb7d917b40a8560ab
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-06-27 14:00:39 +0100

    Fighting Dojo Map changes
    Needs Hitmontop placed in the middle

--- data/maps/headers/FightingDojo.asm
+++ data/maps/headers/FightingDojo.asm
@@ -1,2 +1,2 @@
-	map_header FightingDojo, FIGHTING_DOJO, DOJO, 0
+	map_header FightingDojo, FIGHTING_DOJO, PREGYM, 0
 	end_map_header
--- data/maps/objects/FightingDojo.asm
+++ data/maps/objects/FightingDojo.asm
@@ -8,12 +8,12 @@ FightingDojo_Object:
 	def_bg_events
 
 	def_object_events
-	object_event  5,  3, SPRITE_HIKER, STAY, DOWN, 1, OPP_BLACKBELT, 1 ; koichi
+	object_event  6,  3, SPRITE_HIKER, STAY, DOWN, 1, OPP_BLACKBELT, 1 ; koichi
 	object_event  3,  4, SPRITE_HIKER, STAY, RIGHT, 2, OPP_BLACKBELT, 9
 	object_event  3,  6, SPRITE_HIKER, STAY, RIGHT, 3, OPP_BLACKBELT, 10
-	object_event  5,  5, SPRITE_HIKER, STAY, LEFT, 4, OPP_BLACKBELT, 11
-	object_event  5,  7, SPRITE_HIKER, STAY, LEFT, 5, OPP_BLACKBELT, 12
-	object_event  4,  1, SPRITE_POKE_BALL, STAY, NONE, 6 ; person
-	object_event  5,  1, SPRITE_POKE_BALL, STAY, NONE, 7 ; person
+	object_event  6,  5, SPRITE_HIKER, STAY, LEFT, 4, OPP_BLACKBELT, 11
+	object_event  6,  8, SPRITE_HIKER, STAY, LEFT, 5, OPP_BLACKBELT, 12
+	object_event  5,  1, SPRITE_POKE_BALL, STAY, NONE, 6 ; person
+	object_event  7,  1, SPRITE_POKE_BALL, STAY, NONE, 7 ; person
 
 	def_warps_to FIGHTING_DOJO
