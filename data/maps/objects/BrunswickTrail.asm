baa94d76ae629e77458fe57247755d655127d733
Martha Schilling <harleyrayquaza@gmail.com>
2023-07-04 12:59:54 +0100

    Functioning Galarian Moltres
    - Moltres now functions correctly, seeing you like a normal trainer would.
    
    - Slightly tidied up Brunswick Trail and Celeste Hill Outside's maps
    
    - Celeste Hill Outside and Cave now play Digda01 as a music track

--- data/maps/objects/BrunswickTrail.asm
+++ data/maps/objects/BrunswickTrail.asm
@@ -4,13 +4,12 @@ BrunswickTrail_Object:
 	def_warp_events
 	warp_event 12,  9, GARNET_CAVERN_1F, 1
 	warp_event 40,  5, BRUNSWICK_GROTTO, 1
-	warp_event 41,  5, BRUNSWICK_GROTTO, 1
 
 	def_bg_events
 
 	def_object_events
 	object_event 23, 11, SPRITE_FAKE_TREE, STAY, NONE, 1, CACTUS, 60 | OW_POKEMON ; person
 	object_event 37, 23, SPRITE_BRUNETTE_GIRL, STAY, DOWN, 2 ; person
-	object_event 50, 16, SPRITE_BIRD, WALK, LEFT_RIGHT, 3 ; person
+	object_event 54,  9, SPRITE_BIRD, STAY, DOWN, 3 ; person
 
 	def_warps_to BRUNSWICK_TRAIL
--- scripts/CelesteHill.asm
+++ scripts/CelesteHill.asm
@@ -8,56 +8,19 @@ CelesteHill_Script:
 	ret
 
 CelesteHill_ScriptPointers:
-	dw GMoltFound
 	dw CheckFightingMapTrainers
 	dw DisplayEnemyTrainerTextAndStartBattle
 	dw EndTrainerBattle
-
-; Galarian Moltres fight script.
-; Unlike other legendary Pokemon, Moltres will engage the player first.
-GMoltFound:
-	CheckEvent EVENT_BEAT_GALARIAN_MOLTRES
-	jr nz, .done
-	ld hl, CelesteHillCoords1
-	call ArePlayerCoordsInArray
-	jp nc, CheckFightingMapTrainers
-	ld a, [wCoordIndex]
-	ld [wcf0d], a
-	xor a
-	ldh [hJoyHeld], a
-	ld a, $f0
-	ld [wJoyIgnore], a
 	
-	ld a, $1
-	ldh [hSpriteIndex], a
-	call SetSpriteMovementBytesToFF
-	ld de, GMoltMovement
-	call MoveSprite
-	
-	ld a, $1
-	ldh [hSpriteIndexOrTextID], a
-	call DisplayTextID
-.done
-	ret
-
-CelesteHillCoords1:
-	dbmapcoord 11, 9
+CelesteHillTrainerHeaders:
+	def_trainers
+GalarianMoltresTrainerHeader:
+	trainer EVENT_BEAT_GALARIAN_MOLTRES, 3, GalarianMoltresBattleText, GalarianMoltresBattleText, GalarianMoltresBattleText
 	db -1 ; end
-
-GMoltMovement:
-	db NPC_MOVEMENT_DOWN
-	db NPC_MOVEMENT_DOWN
-	db -1 ;end
 
 CelesteHill_TextPointers:
 	dw GalarianMoltresText
 	dw CelesteHillSign
-
-CelesteHillTrainerHeaders:
-	def_trainers
-GalarianMoltresTrainerHeader:
-	trainer EVENT_BEAT_GALARIAN_MOLTRES, 0, GalarianMoltresBattleText, GalarianMoltresBattleText, GalarianMoltresBattleText
-	db -1 ; end
 
 GalarianMoltresText:
 	text_asm
