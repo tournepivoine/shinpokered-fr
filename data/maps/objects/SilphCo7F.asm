5fb468fe617fe720a82b113d8f9fe9fb8fb0e8f4
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-09-03 13:49:44 +0100

    Silph Co. additional fixes
    - Fixes the 7F -> 5F warp
    - Removes the unused warp panel, moves the TM for Swords Dance over it, and the Rocket Grunts, accordingly.
    - Fixes the strange machine blocks that had missing tiles, by removing them!

--- data/maps/objects/SilphCo7F.asm
+++ data/maps/objects/SilphCo7F.asm
@@ -3,7 +3,7 @@ SilphCo7F_Object:
 
 	def_warp_events
 	warp_event 16,  0, SILPH_CO_11F, 1
-	warp_event 22,  0, SILPH_CO_5F, 1
+	warp_event 22,  0, SILPH_CO_5F, 2
 ;	warp_event 18,  0, SILPH_CO_ELEVATOR, 1
 	warp_event  5,  7, SILPH_CO_11F, 2
 	warp_event  5,  3, SILPH_CO_3F, 4
@@ -18,10 +18,10 @@ SilphCo7F_Object:
 	object_event 10,  8, SPRITE_ERIKA, STAY, NONE, 4 ; person
 	object_event 13,  1, SPRITE_ROCKET, STAY, DOWN, 5, OPP_ROCKET, 32
 	object_event  2, 13, SPRITE_SCIENTIST, STAY, DOWN, 6, OPP_SCIENTIST, 8
-	object_event 20,  2, SPRITE_ROCKET, STAY, LEFT, 7, OPP_ROCKET, 33
+	object_event 22,  6, SPRITE_ROCKET, STAY, LEFT, 7, OPP_ROCKET, 33
 	object_event 19, 14, SPRITE_ROCKET, STAY, RIGHT, 8, OPP_ROCKET, 34
 	object_event  3,  7, SPRITE_BLUE, STAY, UP, 9 ; person
 	object_event  1,  9, SPRITE_POKE_BALL, STAY, NONE, 10, CALCIUM
-	object_event 24, 11, SPRITE_POKE_BALL, STAY, NONE, 11, TM_SWORDS_DANCE
+	object_event 21, 15, SPRITE_POKE_BALL, STAY, NONE, 11, TM_SWORDS_DANCE
 
 	def_warps_to SILPH_CO_7F
